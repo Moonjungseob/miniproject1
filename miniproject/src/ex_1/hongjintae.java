e3ad8d6356ff512111a3ee074ac0a63d9e68f5f7
Moonjungseob <answjdtjq11@gmail.com>
2024-04-24 17:10:53 +0900

    first commit

---
+++ miniproject/src/ex_1/hongjintae.java
@@ -0,0 +1,10 @@
+package ex_1;
+
+public class hongjintae {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ miniproject/src/ex_3/leehansol.java
@@ -0,0 +1,10 @@
+package ex_3;
+
+public class leehansol {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
